33ba848899c59b25981c1271c434fdf7fb5c18e5
Vladimir Prus <vladimir.prus@gmail.com>
2022-11-23 00:42:09 +0200

    deduplicateWithinPartitions: add implicit function

---
+++ lib/src/main/scala/com/joom/spark/DeduplicateWithinPartitions.scala
@@ -0,0 +1,78 @@
+
+package com.joom.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Strategy
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{Ascending, Attribute, SortOrder, UnsafeProjection}
+import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, UnaryNode}
+import org.apache.spark.sql.catalyst.plans.physical.Partitioning
+import org.apache.spark.sql.execution.{SparkPlan, UnaryExecNode}
+
+/** This module adds 'deduplicateWithinPartition' low-level method for deduplicating data.
+  * The standard `dropDuplicates` method becomes `SortAggregateExec` and it causes
+  * repartioning of the data by all the keys. Support we have data partitioned by
+  * device_id, and we want to deduplicate by (device_id, event_id). Then, we can just
+  * sort each partition by (device_id, event_id), and drop duplicates by linear scan, which is what
+  * this extension does.
+  */
+
+  case class DeduplicateWithinPartitions(sortKeys: Seq[Attribute], comparisonKeys: Seq[Attribute], child: LogicalPlan)
+  extends UnaryNode {
+    override def output: Seq[Attribute] = child.output
+
+  override protected def withNewChildInternal(newChild: LogicalPlan): LogicalPlan = copy(child = newChild)
+}
+
+  case class DeduplicateWithinPartitionsExec(sortKeys: Seq[Attribute], comparisonKeys: Seq[Attribute], child: SparkPlan)
+  extends UnaryExecNode {
+
+    private val ordering = sortKeys.map(SortOrder(_, Ascending))
+
+    override def requiredChildOrdering: Seq[Seq[SortOrder]] = {
+      ordering :: Nil
+    }
+
+    override def outputPartitioning: Partitioning = child.outputPartitioning
+
+    override def outputOrdering: Seq[SortOrder] = ordering
+
+    override protected def doExecute(): RDD[InternalRow] = {
+      val filter = (it: Iterator[InternalRow]) => {
+
+        val keysProjection = UnsafeProjection.create(comparisonKeys, child.output)
+        new Iterator[InternalRow] {
+          var value: Option[InternalRow] = if (it.hasNext) Some(it.next().copy) else None
+          override def hasNext: Boolean = value.isDefined
+
+          override def next(): InternalRow = {
+            val r = value.get
+            val keyValues = keysProjection(r).copy()
+            value = None
+            while (it.hasNext && value.isEmpty) {
+              val nextRow = it.next()
+              val nextKeys = keysProjection(nextRow)
+              if (keyValues != nextKeys) {
+                value = Some(nextRow.copy())
+              }
+            }
+            r
+          }
+        }
+      }
+      child.execute().mapPartitions(filter, false)
+    }
+
+    override def output: Seq[Attribute] = child.output
+
+    override protected def withNewChildInternal(newChild: SparkPlan): SparkPlan = copy(child = newChild)
+  }
+
+  object DeduplicateWithinPartitionsStrategy extends Strategy {
+    def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
+      case DeduplicateWithinPartitions(sortKeys, comparisonKeys, child) =>
+        DeduplicateWithinPartitionsExec(sortKeys, comparisonKeys, planLater(child)) :: Nil
+      case _ => Nil
+    }
+  }
+
--- lib/src/main/scala/com/joom/spark/package.scala
+++ lib/src/main/scala/com/joom/spark/package.scala
@@ -10,6 +10,33 @@ package object spark {
   def millis_to_ts(c: Column) = new Column(MillisToTs(c.expr))
 
   object implicits {
+
+    implicit class DeduplicateWithinPartitionsWrapper(df: DataFrame) {
+      def deduplicateWithinPartitions(columnNames: Seq[String]): DataFrame = {
+        deduplicateWithinPartitions(columnNames, columnNames)
+      }
+      def deduplicateWithinPartitions(sortColumnNames: Seq[String], comparisonColumnNames: Seq[String]): DataFrame = {
+        val sparkSession = df.sparkSession
+        val schema = df.schema
+        val resolver = sparkSession.sessionState.analyzer.resolver
+        val allColumns = df.queryExecution.analyzed.output
+        def resolveColumn(colName: String) = {
+          // It is possibly there are more than one columns with the same name,
+          // so we call filter instead of find.
+          val cols = allColumns.filter(col => resolver(col.name, colName))
+          if (cols.isEmpty) {
+            throw new Exception(
+              s"""Cannot resolve column name "$colName" among (${schema.fieldNames.mkString(", ")})""")
+          }
+          cols
+        }
+        val sortColumns = sortColumnNames.flatMap { resolveColumn(_) }
+        val comparisonColumns = comparisonColumnNames.flatMap { resolveColumn(_) }
+        val e = RowEncoder(df.schema)
+        new DataFrame(sparkSession, DeduplicateWithinPartitions(sortColumns, comparisonColumns, df.queryExecution.analyzed), e)
+      }
+    }
+
     implicit class ExplicitRepartitionWrapper(df: DataFrame) {
       def explicitRepartition(numPartitions: Int, partitionExpression: Column): DataFrame = {
         val sparkSession = df.sparkSession
